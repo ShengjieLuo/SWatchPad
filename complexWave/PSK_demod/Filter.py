63e6fe90c18e65e9d04a597d794504ebd0b752a3
test <326902993@qq.com>
2016-11-17 18:30:55 +0800

    Add files via upload

---
+++ complexWave/PSK_demod/Filter.py
@@ -0,0 +1,25 @@
+from readWave import _readwave
+from readWave import showwave
+import numpy as np
+import csv
+
+def Filter18Khz(xf):
+    len1=len(xf);
+    for i in range(int(len1 * (17.5 / 44)), int(len1 * (18.5 / 44))):
+        xf[i] = 0 + 0.00000000e+00j;  # remove the 17Khz in Frequency domain
+    for i in range(int(len1 * (17.5 / 44))):
+        xf[i] = 0 + 0.00000000e+00j;
+
+    for i in range(int(len1 * (18.5 / 44)),len1):
+        xf[i] = 0 + 0.00000000e+00j;
+    return xf
+
+
+def Filter17Khz(xf):
+    len1=len(xf);
+    for i in range(int(len1 * (16.5 / 44))):
+        xf[i] = 0 + 0.00000000e+00j;
+
+    for i in range(int(len1 * (17.5 / 44)), len1):
+        xf[i] = 0 + 0.00000000e+00j;
+    return xf
