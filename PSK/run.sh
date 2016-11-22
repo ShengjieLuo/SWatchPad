f9dc0f5278376aaee0f6530fc51417f14e6dae4b
ShengjieLuo <luoshengjie@sjtu.edu.cn>
2016-11-22 22:33:36 +0800

    new files today

--- PSK/run.sh
+++ PSK/run.sh
@@ -1,6 +1,6 @@
 #python PSKmain.py ../testwave/11-18/Name18No.wav.wav 11.95 44100 10 1
 #python PSKmain.py ../testwave/11-18/Name18L.wav.wav 11.95 44100 10 1
-python PSKmain.py ../testwave/11-18/Name18M.wav.wav 11.95 44100 10 1
+#python PSKmain.py ../testwave/11-18/Name18M.wav.wav 11.95 44100 10 1
 #python PSKmain.py ../testwave/11-18/Name18H.wav.wav 11.95 44100 10 1
 #python PSKmain.py ../testwave/11.21/sameNohand1.wav 11.95 44100 10 1
 #python PSKmain.py ../testwave/11.21/sameNohand2.wav 11.95 44100 10 1
@@ -9,3 +9,9 @@ python PSKmain.py ../testwave/11-18/Name18M.wav.wav 11.95 44100 10 1
 #python PSKmain.py ../testwave/11.21/sameNohand5.wav 11.95 44100 10 1
 #python PSKmain.py ../makewave/18000Hz_10s_PSKSequenceZeroTwoPath.wav 11.95 44100 10 1
 #python PSKmain.py ../makewave/18000Hz_10s_1.3p_PSKSequenceZeroTwoPath.wav 11.95 44100 10 1
+
+#python PSKmain.py ../testwave/11.22/Name22nohand.wav.wav 11.95 44100 10 1
+#python PSKmain.py ../testwave/11.22/Name22nohand2.wav.wav 11.95 44100 10 1
+#python PSKmain.py ../testwave/11.22/Name22nohand3.wav.wav 11.95 44100 10 1
+#python PSKmain.py ../testwave/11.22/Name22hand1.wav.wav 11.95 44100 10 1
+python PSKmain.py ../testwave/11.22/Name22hand2.wav.wav 11.95 44100 10 1
