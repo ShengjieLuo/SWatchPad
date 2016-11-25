bc883c8d8db1f26cb7f4addac904e416404630b9
ShengjieLuo <luoshengjie@sjtu.edu.cn>
2016-11-25 13:45:58 +0800

    Adapt the additional microphone sample

--- environment/change.sh
+++ environment/change.sh
@@ -7,4 +7,15 @@
 #python changeAdditionalMicro.py ../testwave/11-15/hand/1503hand.wav ../testwave/11-15/hand/Base1503hand.wav
 #python changeAdditionalMicro.py ../testwave/11-15/hand/1504hand.wav ../testwave/11-15/hand/Base1504hand.wav
 
-python changeAdditionalMicro.py ../testwave/11-17/mic.wav ../testwave/11-17/PSKmic.wav
+#python changeAdditionalMicro.py ../testwave/11-17/mic.wav ../testwave/11-17/PSKmic.wav
+python changeAdditionalMicro.py ../testwave/11.25/25high1.wav ../testwave/11.25/Base25high1.wav
+python changeAdditionalMicro.py ../testwave/11.25/25high2.wav ../testwave/11.25/Base25high2.wav
+python changeAdditionalMicro.py ../testwave/11.25/25mid1.wav ../testwave/11.25/Base25mid1.wav
+python changeAdditionalMicro.py ../testwave/11.25/25mid2.wav ../testwave/11.25/Base25mid2.wav
+python changeAdditionalMicro.py ../testwave/11.25/25low1.wav ../testwave/11.25/Base25low1.wav
+python changeAdditionalMicro.py ../testwave/11.25/25low2.wav ../testwave/11.25/Base25low2.wav
+python changeAdditionalMicro.py ../testwave/11.25/25nohand1.wav ../testwave/11.25/Base25nohand1.wav
+python changeAdditionalMicro.py ../testwave/11.25/25nohand2.wav ../testwave/11.25/Base25nohand2.wav
+python changeAdditionalMicro.py ../testwave/11.25/25nohand3.wav ../testwave/11.25/Base25nohand3.wav
+
+
